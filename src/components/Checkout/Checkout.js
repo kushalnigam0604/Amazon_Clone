1a4f5a162e21dedac8ec1c8ca59b97ea498c3f63
kushalnigam0604 <nigamkushal0@gmail.com>
2023-02-27 23:28:17 +0530

    Add checkoutProduct component

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import './Checkout.css';
-import checkout_Ad from '../../assets/checkout_Ad.jpg';
-import Subtotal from '../Subtotal/Subtotal';
+import React from "react";
+import "./Checkout.css";
+import checkout_Ad from "../../assets/checkout_Ad.jpg";
+import Subtotal from "../Subtotal/Subtotal";
+import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
+import { useStateValue } from "../../Context/StateProvider";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
-    <div className='checkout'>
+    <div className="checkout">
       <div className="checkout__left">
-          <img className='checkout_Ad' src={checkout_Ad} alt="" />
-          <h2 className="checkout__title">
-              Your Shopping Basket
-          </h2>
+        <img className="checkout_Ad" src={checkout_Ad} alt="" />
+        <div>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
+          {basket.map(item => (
+          <CheckoutProduct 
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+          />
+          ))}
+        </div>
       </div>
       <div className="checkout__right">
-          <Subtotal/>
+        <Subtotal />
       </div>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
